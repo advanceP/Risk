ca2bc120440978dd570cd8fb7b5e36a6776298bc
advanceP <shahryar.haghighi.fard@gmail.com>
2018-11-28 20:25:15 -0500

    test to see if weather benevolent player attack or not(should not
    attack)

--- Risk/src/test/AgreessiveTest.java
+++ Risk/src/test/AgreessiveTest.java
@@ -9,8 +9,11 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 public class AgreessiveTest {
 
@@ -54,4 +57,22 @@ public class AgreessiveTest {
         assertTrue(nodes.containsAll(nodes2));
         assertTrue(nodes2.containsAll(nodes));
     }
-}
+    /**
+     * test to see if weather benevolent player attack or not(should not attack)
+     */
+    @Test
+    public void testBenevolentAttack() {
+        driver.setPlayers(2);
+        List<String> str = new ArrayList<>();
+        str.add("Benevolent");
+        str.add("Benevolent");
+        driver.giveStrategieToPlayer(str);
+        Player currentPlayer = driver.getCurrentPlayer();
+        List<Node> nodeList = currentPlayer.getNodeList();
+        nodeList.get(0).setArmies(10);
+        //currentPlayer.executeStrategyRein(null);
+        boolean attack = currentPlayer.attack(null, null, null, null);
+        assertFalse(attack);
+        
+    }
+    }
